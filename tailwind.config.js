bc89602b91076d3f5e5965f48cbfe383bbf4cf92
Emmanuel López <emmlop243@gmail.com>
2022-05-25 09:49:27 -0500

    Commit inicial del proyecto y de tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      black: '#000000',
+      white: '#ffffff',
+      gray: '#656565',
+      'gray-middle': '#9B9B9B',
+      'gray-light': '#B4B4B4',
+      'gray-lightest': '#E4E4E4',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
