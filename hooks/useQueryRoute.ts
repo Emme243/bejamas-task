819d236862a024cc4e517aa3c0b3b070fb13d133
Emmanuel López <emmlop243@gmail.com>
2022-05-30 11:49:59 -0500

    useQueryRoute.ts: Se arregló la actualización de los query params en el route, ahora se pueden vaciar tanto las categorías como el rango de precios

--- hooks/useQueryRoute.ts
+++ hooks/useQueryRoute.ts
@@ -13,33 +13,31 @@ function useQueryRoute() {
   const router = useRouter();
 
   function setQueryParamsToUrl(query: Query): void {
-    const newQuery = { ...router.query, ...query };
-    const queryString = Object.entries(newQuery)
+    const queryString = Object.entries(query)
       .map(([key, value]) => `${key}=${value}`)
       .join('&');
     router.push(`/?${queryString}`, undefined, { shallow: true });
   }
 
   function setCategoriesToUrl(categories: string[]) {
-    const query: Query = { categories: categories.join(',') };
-    if (categories.length === 0) delete query.categories;
-    setQueryParamsToUrl(query);
+    const newQuery: Query = { ...router.query, categories: categories.join(',') };
+    if (categories.length === 0) delete newQuery.categories;
+    setQueryParamsToUrl(newQuery);
   }
 
   function setCurrentPageToUrl(page: number) {
-    const query: Query = { currentPage: page.toString() };
-    if (!page) delete query.currentPage;
-    setQueryParamsToUrl(query);
+    const newQuery: Query = { ...router.query, currentPage: page.toString() };
+    setQueryParamsToUrl(newQuery);
   }
 
   function setPriceRangeToUrl(priceRange: number[]) {
-    const query: Query = { priceRange: priceRange.join(',') };
-    if (priceRange.length === 0) delete query.priceRange;
-    setQueryParamsToUrl(query);
+    const newQuery: Query = { ...router.query, priceRange: priceRange.join(',') };
+    if (priceRange.length === 0) delete newQuery.priceRange;
+    setQueryParamsToUrl(newQuery);
   }
 
   function setSortingToUrl(sortBy: string, sortType: SortType) {
-    const query: Query = { sortBy, sortType };
+    const query: Query = { ...router.query, sortBy, sortType };
     setQueryParamsToUrl(query);
   }
 
