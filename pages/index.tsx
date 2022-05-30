0eba8f0b2774c7f485f62b7fa0cce4346aeab23a
Emmanuel López <emmlop243@gmail.com>
2022-05-30 15:57:24 -0500

    Se añadió el tag title a la página index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import Navbar from '../components/Ui/Navbar';
 import FeaturedArtworkSection from '../components/HomeSections/FeaturedArtworkSection';
 import ArtworkSection from '../components/HomeSections/ArtworkSection';
+import Head from 'next/head';
 
 const Home = () => {
   return (
     <div className="my-container mb-7">
+      <Head>
+        <title>Bejamas Task | Emmanuel</title>
+      </Head>
+
       <Navbar className="sticky top-0 z-30" />
       <div className="mt-4">
         <FeaturedArtworkSection />
